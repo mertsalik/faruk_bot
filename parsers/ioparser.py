630bf4fa017f5b021629c91ceb50c8aae05939ef
Mert Salık <mertsalik.itu@gmail.com>
2018-01-04 23:13:08 +0300

    init

---
+++ parsers/ioparser.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "mertsalik"
+__copyright__ = "Copyright 2018"
+__credits__ = ["mertsalik", ""]
+__license__ = "Private"
+__email__ = ""
+
+
+class IOParser(object):
+    """
+    Import settings from engine to bot
+    """
+
+    @staticmethod
+    def parse(input_string):
+        _, key, value = input_string.split()
+        return {key: value}
+
+
+    def wait(self):
