458dcdd885cd9028e463aa5221ab4bd68a4a6a82
Mert Salık <mertsalik.itu@gmail.com>
2018-01-05 02:43:57 +0300

    #todo: create from 2dim array

--- models/graph.py
+++ models/graph.py
@@ -1,7 +1,26 @@
 # -*- coding: utf-8 -*-
+from collections import defaultdict
 
 __author__ = "mertsalik"
 __copyright__ = "Copyright 2018"
 __credits__ = ["mertsalik", ""]
 __license__ = "Private"
 __email__ = ""
+
+
+class Graph(object):
+    def __init__(self):
+        self.nodes = set()
+        self.edges = defaultdict(list)
+        self.distances = {}
+
+    def add_node(self, value):
+        self.nodes.add(value)
+
+    def add_edge(self, source, destination, distance):
+        self.edges[source].append(destination)
+        self.edges[destination].append(source)
+        self.distances[(source, destination)] = distance
+
+    def from_matrix(self, matrix):
+        raise NotImplementedError
