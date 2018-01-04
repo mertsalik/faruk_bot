d330dc5a4c0fe9f6680b2e886ffd9fb7568c91a1
Mert Salık <mertsalik.itu@gmail.com>
2018-01-05 01:37:43 +0300

    parse field and generate field & cells

--- parsers/fieldparser.py
+++ parsers/fieldparser.py
@@ -5,3 +5,15 @@ __copyright__ = "Copyright 2018"
 __credits__ = ["mertsalik", ""]
 __license__ = "Private"
 __email__ = ""
+
+
+class FieldParser(object):
+    matrix = []
+
+    def __init__(self, input_string, width, height):
+        cells = input_string.split(',')
+        self.matrix = [cells[x:x + width] for x in
+                       range(0, len(cells), width)]
+
+    def get_matrix(self):
+        return self.matrix
